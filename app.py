bc9083a5ba89adc0f8d732632b64f80f62071087
prachipatidar1312-png <prachipatidar1312@gmail.com>
2026-04-24 11:02:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,6 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render uses this
+    app.run(host="0.0.0.0", port=port)
